aea8525a7658b725be27af3ba94630189af9c59f
KhangNguyenPham <P.khangsnsd@gmail.com>
2021-07-25 01:06:26 +0700

    link button all sale

--- resources/js/components/Home.js
+++ resources/js/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Carousel2 from "./Carousel2";
 import Featured_Books from "./Featured_Books";
+import {Link} from "react-router-dom";
 
 function Home(){
     return(
@@ -10,7 +11,7 @@ function Home(){
                     <h4>On Sale</h4>
                 </div>
                 <div className="col">
-                    <button className="btn btn-secondary view-all">View All</button>
+                    <button className="btn btn-secondary view-all"><Link to="/Shop" className="text-white-color">View All</Link></button>
                 </div>
             </div>
             <Carousel2 />
